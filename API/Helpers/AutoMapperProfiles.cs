db8e72c83cef11f9968c601f1280e57e1685c2b1
CONSTANTIN CATALIN MARIN <constantin.marin@s.unibuc.ro>
2022-01-31 13:09:35 +0200

    Chat Functionality

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -22,6 +22,12 @@ namespace API.Helpers
             CreateMap<MemberUpdateDto, AppUser>();
             
             CreateMap<RegisterDto, AppUser>();
+
+            CreateMap<Message, MessageDto>()
+                .ForMember(dest => dest.SenderPhotoUrl, opt => opt.MapFrom(src => 
+                    src.Sender.Photos.FirstOrDefault(X => X.IsMain).Url))
+                .ForMember(dest => dest.RecipientPhotoUrl, opt => opt.MapFrom(src => 
+                    src.Recipient.Photos.FirstOrDefault(X => X.IsMain).Url));
         }
     }
 }
